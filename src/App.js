111698f9a4dbaddc6da61619d7b4d90057b8347a
arnaud guillemin <arnaud.guillemin27@gmail.com>
2024-07-16 18:20:01 +0200

    création du composant header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/header.jsx'; // Assurez-vous d'importer correctement votre composant Header
 
 function MyComponent() {
   return (
     <div>
-      <h1>My Component</h1>
+      <Header /> {/* Remplace <h1>My Component</h1> par votre Header */}
     </div>
   );
 }
