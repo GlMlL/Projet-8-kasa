49c66d3ffb6c6d2e224637b1e793aa641fabce2c
arnaud guillemin <arnaud.guillemin27@gmail.com>
2024-07-31 11:40:07 +0200

    modif components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/header.jsx';
-import Home from './components/home';
+import Header from './components/Header.jsx';
+import Home from './components/Home';
 import About from './pages/About';
 import Annonce from "./components/Annonce";
-import Error from './components/error';
-import {Footer} from "./components/footer";
+import Error from './components/Error';
+import {Footer} from "./components/Footer";
 
 function MyComponent() {
     return (
